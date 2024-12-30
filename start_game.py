18bafe556b544f927378d578ac63b98b18e448cb
HillaryKchao <hillary.kchao@gmail.com>
2024-12-30 01:01:30 -0800

    Single player functionality added (no timer though)

--- start_game.py
+++ start_game.py
@@ -1,2 +1,10 @@
-import * from word_finder
+from word_finder import *
+from game import *
 
+board = Board()
+board.populate()
+
+player_list = []
+game = Game(player_list, board)
+player = Player(game, 0)
+game.player_list.append(player)
