507b0da2a679ac57a1b9aa824ac9a13faa394f4b
bkunzang <broob@blui.com>
2025-01-01 15:02:51 -0800

    minor changes

--- boggle_analysis.py
+++ boggle_analysis.py
@@ -8,25 +8,27 @@ plt.rcParams['figure.dpi'] = 150
 plt.rcParams['savefig.dpi'] = 150
 
 
-board = Board()
+board = Board(dim=5)
 
 def simulate():
     result = []
     for i in range(10000):
         board.populate()
         result.append(board.total_points)
-        if board.total_points >= 700:
-            print(board, board.all_words, board.total_points)
+        #if board.total_points >= 700:
+         #   print(board, board.all_words, board.total_points)
     np_result = np.array(result)
     return np_result
 
 sim = simulate()
 x=stats.describe(sim)
 y=sns.histplot(sim)
+med=np.median(sim)
 z = stats.norm.interval(0.99, loc = np.mean(sim), scale = np.std(sim) / 100)
-plt.title('Distribution of Points in Standard Boggle')
+plt.title('Distribution of Points in Big Boggle')
 plt.ylabel('Frequency')
 plt.xlabel('Points')
 plt.show()
 print(x)
 print(z)
+print(med)
